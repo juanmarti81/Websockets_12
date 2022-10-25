b48e93dfd73db7f212276b2b9f21587406a987df
Juan Marti <juanma.marti@gmail.com>
2022-10-25 00:17:39 -0300

    Chat added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const Contenedor = require("./productsClass")
-
 const express = require("express")
 const cors = require("cors")
 const {Server: HttpServer} = require("http")
 const {Server: WebsocketServer} = require("socket.io")
 
-const handlebars = require("express-handlebars") 
+// const handlebars = require("express-handlebars") 
+const messageDB = require("./messagesClass")
+const Contenedor = require("./productsClass")
 
 let contenedor = new Contenedor()
+let messageList = new messageDB()
 
 const app = express()
 const httpServer = new HttpServer(app)
@@ -29,48 +30,63 @@ app.use(express.static("./public"))
 // app.set("view engine", "hbs")
 // app.set("views", "./views")
 
-app.get("/", (req, res) => {
-  console.log("Respuesta")
+// app.get("/", (req, res) => {
+//   console.log("Respuesta")
   
-  res.render("new-product")
-})
+//   res.render("new-product")
+// })
 
-app.get("/productos", async (req, res) => {
-  const products = await contenedor.getAll()
-  res.render("list-products", {product: products})
-})
+// app.get("/productos", async (req, res) => {
+//   const products = await contenedor.getAll()
+//   res.render("list-products", {product: products})
+// })
 
-app.post("/productos", async (req, res) => {
-  const newProduct = req.body 
-  console.log(newProduct)
-  const save = await contenedor.save(newProduct)
-  const products = await contenedor.getAll()
-  res.render("list-products", {product: products})
-})
+// app.post("/productos", async (req, res) => {
+//   const newProduct = req.body 
+//   console.log(newProduct)
+//   const save = await contenedor.save(newProduct)
+//   const products = await contenedor.getAll()
+//   res.render("list-products", {product: products})
+// })
 
-app.get("/productoRandom", async (req, res) => {
-  const products = await contenedor.getAll()
-  const length = Object.keys(products).length
-  const productIndexSelected = Math.floor(Math.random() * length)
-  console.log("VALUE: ", productIndexSelected)
-  console.log("PRODUCT: ", products[productIndexSelected])
-  res.status(200).send(products[productIndexSelected])
-})
+// app.get("/productoRandom", async (req, res) => {
+//   const products = await contenedor.getAll()
+//   const length = Object.keys(products).length
+//   const productIndexSelected = Math.floor(Math.random() * length)
+//   console.log("VALUE: ", productIndexSelected)
+//   console.log("PRODUCT: ", products[productIndexSelected])
+//   res.status(200).send(products[productIndexSelected])
+// })
 
-let Users = []
 
 const getProducts = async () => {return await contenedor.getAll()}
 
+
 io.on("connection", socket => {
   returnProducts(socket)
+  returnMessages(socket)
 
   socket.on("new-product", data => {
     console.log(data)
     saveProduct(data)
     
   })
+
+  socket.on("new-message", data => {
+    saveMessage(data)
+  })
   
 })
+
+const returnMessages = async (socket) => {
+  socket.emit("AllMessages", await messageList.getAllMessages())
+}
+
+const saveMessage = async (data) => {
+  await messageList.save(data)
+  const messages = await messageList.getAllMessages()
+  io.sockets.emit("AllMessages", messages)
+}
 
 const returnProducts = async (socket) => {
   socket.emit("AllProducts", await getProducts())
--- public/lib/clientSocket.js
+++ public/lib/clientSocket.js
@@ -1,7 +1,10 @@
-const socket = io("http://localhost:8080")
+// const socket = io("http://localhost:8080")
+const socket = io("http://150.136.145.245:8080")
 
 const NuevoProducto = document.getElementById("Nuevo_Producto")
 const showProducts = document.getElementById("productList")
+const NuevoMensaje = document.getElementById("Nuevo_Mensaje")
+const showMessages = document.getElementById("messages_List")
 
 NuevoProducto.addEventListener("submit", (e) => {
   e.preventDefault()
@@ -12,17 +15,37 @@ NuevoProducto.addEventListener("submit", (e) => {
   console.log(values)
 })
 
+
+
 socket.on("AllProducts", data => {
   productList(data)
 })
 
 const productList = async (product) => {
   const res = await fetch("./list-products.hbs")
-  console.log(res)
   const temp = await res.text() 
-  var template = Handlebars.compile(temp);
+  const template = Handlebars.compile(temp);
   const html = template({product})
   showProducts.innerHTML = html
 }
 
+NuevoMensaje.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const data = new FormData(NuevoMensaje)
+  const values = Object.fromEntries(data)
+  NuevoMensaje.reset()
+  socket.emit("new-message", values)
+  console.log(values)
+})
 
+socket.on("AllMessages", data => {
+  MessageList(data)
+})
+
+const MessageList = async (messages) => {
+  const res = await fetch("./chat.hbs")
+  const temp = await res.text()
+  const template = Handlebars.compile(temp)
+  const html = template({messages})
+  showMessages.innerHTML = html
+}
